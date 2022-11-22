36ac7323ecc3a7474f616130da0ac337dcf05197
Marc PEYSALE <marc.peysale@niji.fr>
2022-11-22 15:36:49 +0100

    Added Circular Progress Indicator on MainActivity to show end user that network request is ongoing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
@@ -16,11 +18,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var planetsAdapter: PlanetsAdapter
 
+    private lateinit var circularProgressIndicator: CircularProgressIndicator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recycler_view)
+        circularProgressIndicator = findViewById(R.id.circular_progress_indicator)
 
         val url = URL("https://swapi.dev/api/planets")
 
@@ -30,10 +35,13 @@ class MainActivity : AppCompatActivity() {
 
         val client = OkHttpClient()
 
+        circularProgressIndicator.visibility = View.VISIBLE
+
         client.newCall(request).enqueue(object : Callback {
 
             override fun onFailure(call: Call, e: IOException) {
                 Log.e("OKHTTP", e.localizedMessage)
+                circularProgressIndicator.visibility = View.GONE
             }
 
             override fun onResponse(call: Call, response: Response) {
@@ -42,6 +50,7 @@ class MainActivity : AppCompatActivity() {
                     val planetsResponse = gson.fromJson(it, PlanetsResponse::class.java)
                     planetsResponse.planets?.let { it1 ->
                         runOnUiThread {
+                            circularProgressIndicator.visibility = View.GONE
                             planetsAdapter = PlanetsAdapter(it1)
                             recyclerView.adapter = planetsAdapter
                             recyclerView.layoutManager = LinearLayoutManager(applicationContext)
